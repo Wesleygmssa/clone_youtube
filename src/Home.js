5944ca5991a0fe04449ac04ea6cbd256fc582c1c
wesley guerra <guerra_ssa@hotmail.com>
2021-03-10 22:36:22 -0300

    Criando menu lateral

--- src/Home.js
+++ src/Home.js
@@ -1,81 +1,132 @@
-import { AppBar, Button, IconButton, makeStyles,  Toolbar } from '@material-ui/core';
-import React from 'react';
-import MenuIcon from '@material-ui/icons/Menu';
-import {  AccountCircle, MoreVert} from '@material-ui/icons';
-import Apps from '@material-ui/icons/Apps';
-import VideoCall from '@material-ui/icons/VideoCall';
+import {
+  AppBar,
+  Button,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+  Toolbar,
+} from "@material-ui/core";
+import React from "react";
+import MenuIcon from "@material-ui/icons/Menu";
+import { AccountCircle, MoreVert } from "@material-ui/icons";
+import Apps from "@material-ui/icons/Apps";
+import VideoCall from "@material-ui/icons/VideoCall";
 
 //criando classes
-const useStyles = makeStyles((theme) =>({
-    root: {
-      height: '100vh',
-    
-    },
-    appBar: {
-        boxShadow: 'none',
-    },
-    grow:{
-        flexGrow: 1
-    },
-    icons:{
-       paddingRight: theme.spacing(5),
-    },
-    menuIcon:{
-        paddingRight: theme.spacing(5),
-        paddingLeft: theme.spacing(6)
-    },
-    logo:{
-        
-    }
-    
-  })); 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+  appBar: {
+    boxShadow: "none",
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  icons: {
+    paddingRight: theme.spacing(5),
+  },
+  menuIcon: {
+    paddingRight: theme.spacing(5),
+    paddingLeft: theme.spacing(6),
+  },
+  logo: {
+    height: "30px",
+  },
+}));
 
-const Home = () =>{
-    const classes = useStyles();
-    return(
-        <div className={classes.root}>
-            <AppBar color="inherit" className={classes.appBar} >
-             <Toolbar>
+const Home = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar color="inherit" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuIcon}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <img src="/images/preto.png" alt="logo" className={classes.logo} />
 
-                 <IconButton
-                 edge="start" 
-                 className={classes.menuIcon} 
-                 color="inherit" 
-                 aria-label="menu">
-                  <MenuIcon />
-                 </IconButton>
-                 <img src="/images/preto.png" alt="logo"  className={classes.logo}/>
+          <div className={classes.grow}></div>
 
-                    <div className={classes.grow}></div>
+          <IconButton
+            edge="start"
+            className={classes.icons}
+            color="inherit"
+            aria-label="menu"
+          >
+            <VideoCall />
+          </IconButton>
 
-                    <IconButton
-                 edge="start" 
-                 className={classes.icons} 
-                 color="inherit" 
-                 aria-label="menu">
-                  <VideoCall />
-                 </IconButton>
+          <IconButton
+            edge="start"
+            className={classes.icons}
+            color="inherit"
+            aria-label="menu"
+          >
+            <Apps />
+          </IconButton>
 
-                 <IconButton
-                 edge="start" 
-                 className={classes.icons} 
-                 color="inherit" 
-                 aria-label="menu">
-                  <Apps />
-                 </IconButton>
+          <IconButton
+            edge="start"
+            className={classes.icons}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MoreVert />
+          </IconButton>
 
-                 <IconButton
-                 edge="start" 
-                 className={classes.icons} 
-                 color="inherit" 
-                 aria-label="menu">
-                  <MoreVert />
-                 </IconButton>
-                    
-                    <Button startIcon={<AccountCircle />} variant="outlined" color="secondary">Fazer Login</Button>
-             </Toolbar>
-            </AppBar>
+          <Button
+            startIcon={<AccountCircle />}
+            variant="outlined"
+            color="secondary"
+          >
+            Fazer Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <Toolbar />
+        <div className={classes.drawerContainer}>
+          <List>
+            {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {["All mail", "Trash", "Spam"].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
         </div>
-    )
-}
+      </Drawer>
+    </div>
+  );
+};
 export default Home;
